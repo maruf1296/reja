f47adbdf5ddb27f13f0f2e4e7f73025ce6da2062
Maruf <marufoo1996@gmail.com>
2025-06-06 21:11:35 +0900

    fix: fix error

--- train.js
+++ train.js
@@ -1,1 +1,93 @@
-console.log('train task ishga tushdi!');
+console.log("train task ishga tushdi!");
+// // =================================================
+// console.log("Jck Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling",
+//   "togri boshliq tanlang va koproq hato qiling",
+//   "uzingizga ishlashingizni boshlang",
+//   "siz kuchli bolgan narsalarni qiling",
+//   "yoshlarga investitsiya qiling",
+//   "endi dam oling, foydasi yoq",
+// ];
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("raqam kiriting", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     callback(null, list[5]);
+//   }
+// }
+
+// maslahatlariBering(2, (err, data) => {
+//   if (err) console.log("error:", err);
+// //   console.log("javob:", data);
+// // });
+
+// // =================================================
+// console.log("Jck Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling",
+//   "togri boshliq tanlang va koproq hato qiling",
+//   "uzingizga ishlashingizni boshlang",
+//   "siz kuchli bolgan narsalarni qiling",
+//   "yoshlarga investitsiya qiling",
+//   "endi dam oling, foydasi yoq",
+// ];
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("raqam kiriting");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       },5000);
+//     });
+//   }
+// }
+
+
+    // setTimeout (fubction (){
+    //  return list[5];
+    // },5000);
+  
+
+//then/catch
+// console.log("passed here 0");
+// maslahatBering(89).
+//    then(data => {
+//          console.log("javob:", data);
+// }).catch(err => {
+//          console.log("ERROR:", err);
+// })
+// console.log("passed here 1");
+
+
+// // asyn/await
+// async function run() {
+//   let javob = await maslahatBering(25);
+//   console.log(javob);
+//   javob = await maslahatBering(65);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
+
+
+function bambboo(e, element) {
+  let javob = element.split("");
+  let javob1 = javob.filter((q) => q === e);
+  console.log(`${element} ning ichida ${javob1.length} ta ${e} bor`);
+}
+
+bambboo("b", "bambboo");
