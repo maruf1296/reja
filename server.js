7e5ad9d45d3a8e54ac8e925f0d6318ab00593195
Maruf <marufoo1996@gmail.com>
2025-06-08 15:25:31 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,52 +1,30 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/use.json", "utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://cluster0.jfzyptt.mongodb.net/reja?retryWrites=true&w=majority&appName=Cluster0";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+
+        const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-//1: Krish kodlari
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//2: Session kodlar
-
-//3: Viewe kodlar
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4: Routing kodlar
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style="background: yellow">HELLO WORLD by Maruf</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
-// });
-app.post("/create-item", (req, res) => {
-console.log(req.body);
-res.json({test: "success"});
-});
-
-app.get("/author",(req, res) => {
-  res.render("author", {user: user});
-});
-
-app.get("/", function(req, res) {
-res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
+);
