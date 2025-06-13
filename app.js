5613fc56a2646745ee48995b844cde2143a91a51
Maruf <marufoo1996@gmail.com>
2025-06-13 15:41:08 +0900

    feat: FrontEnd JSni qurish

--- app.js
+++ app.js
@@ -29,26 +29,21 @@ app.set("view engine", "ejs");
 //4: Routing kodlar
 
 app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item");
+  console.log(req.body);
   const new_reja = req.body.reja;
-  db.collection("plans").insertOne({ reja: new_reja }, (err, date) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+   res.json(data.ops[0] );
   });
 });
 
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
+// app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({ test: "success" });
+// });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+// app.get("/author", (req, res) => {
+//   res.render("author", { user: user });
+// });
 
 app.get("/", function (req, res) {
   console.log("user entered /");
