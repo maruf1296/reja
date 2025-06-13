e5bb1e2c73fc934400ccafb7e87da6514b444bb3
Maruf <marufoo1996@gmail.com>
2025-06-13 17:32:40 +0900

    NV: Rejalarni o'chirish (DELETE)

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ fs.readFile("database/use.json", "utf8", (err, data) => {
 
 //MongoDB connect chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 //1: Krish kodlari
 app.use(express.static("public"));
@@ -34,6 +35,13 @@ app.post("/create-item", (req, res) => {
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
    res.json(data.ops[0] );
   });
+});
+
+app.post("/delete-item", (req, res) => {
+const id = req.body.id;
+db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
+  res.json({state: "success"});
+})
 });
 
 // app.post("/create-item", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -28,3 +28,27 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       console.log("Iltimos qaytadan harakat qiling");
     });
 });
+
+
+document.addEventListener("click", function (e) {
+    //delete oper
+    console.log(e.target);
+    if (e.target.classList.contains("delete-me")) {
+        if(confirm("Anniq ochirmoqchimisiz?")) {
+          axios.post("delete-item", {id: e.target.getAttribute("data-id")})
+          .then((response) => {
+            console.log(response.data);
+            e.target.parentElement.parentElement.remove();
+          })
+          .catch((err) => {
+            console.log("Iltimos qaytadan harakat qiling");
+          });
+         }
+    }
+        
+
+    //edit oper
+    if (e.target.classList.contains("edit-me")) {
+        alert("siz edit tugmasini bosdingiz");
+    }
+})
